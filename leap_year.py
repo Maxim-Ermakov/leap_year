eb7eee1e41458d0ee3d3738d03f1ade3bc92615d
Maxim-Ermakov <prospethee@gmail.com>
2024-01-10 12:32:33 +0300

    second commit

--- leap_year.py
+++ leap_year.py
@@ -1,7 +1,7 @@
 def leap_year():
     year = int(input("Введите год: "))
 
-    if year // 4 and year // 100:
+    if year % 4 == 0:
         print("Год является високосным.")
     else:
         print("Год не является високосным.")
