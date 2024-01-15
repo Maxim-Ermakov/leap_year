0fbe3fff66e408d04c7eefce7845e4216fce7859
Maxim-Ermakov <prospethee@gmail.com>
2024-01-15 10:19:06 +0300

    Error handler

--- leap_year.py
+++ leap_year.py
@@ -1,10 +1,14 @@
-def leap_year():
-    year = int(input("Введите год: "))
+try:
+    def leap_year():
+        year = int(input("Введите год: "))
 
-    if year % 4 == 0 and year % 400 == 0:
-        print("Год является високосным.")
-    else:
-        print("Год не является високосным.")
+        if year % 4 == 0 and year % 400 == 0:
+            print("Год является високосным.")
+        else:
+            print("Год не является високосным.")
 
+except ValueError:
+    print('Вводите только год и без пробелов!')
 
-leap_year()
+else:
+    leap_year()
